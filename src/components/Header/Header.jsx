41ec789dcf8625c9c0ad34cd159b055ffdc22acf
olyurmanova <ol.yurmanova@gmail.com>
2022-06-15 13:49:30 +0300

    Update dependencies, fix active link class

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,30 +1,48 @@
-import React from 'react';
-import {NavLink} from "react-router-dom";
-import {useDispatch, useSelector} from "react-redux";
-import {logout} from "../../redux/authReducer";
-import {Box} from "@material-ui/core";
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../../redux/authReducer";
+import { Box } from "@material-ui/core";
 import classes from "./Header.module.scss";
-import {ReactComponent as LogoutIcon} from "../../assets/images/logout-icon.svg";
+import { ReactComponent as LogoutIcon } from "../../assets/images/logout-icon.svg";
 
 const Header = () => {
   const dispatch = useDispatch();
-  const isAuth = useSelector(state => state.auth.isAuth);
+  const isAuth = useSelector((state) => state.auth.isAuth);
 
   return (
     <header className={classes.header}>
       <nav className={classes.nav}>
         <Box className={classes.linkWrapper}>
-          <NavLink className={classes.link} activeClassName={classes.active} to={"/"}  exact>
+          <NavLink
+            className={(navData) =>
+              navData.isActive
+                ? `${classes.link} ${classes.active}`
+                : classes.link
+            }
+            to={"/"}
+            exact
+          >
             Home
           </NavLink>
           {isAuth && (
-            <button className={`${classes.link} ${classes.button}`} onClick={() => dispatch(logout())}>
+            <button
+              className={`${classes.link} ${classes.button}`}
+              onClick={() => dispatch(logout())}
+            >
               <LogoutIcon className={classes.buttonIcon} />
               <span>Logout</span>
             </button>
           )}
         </Box>
-        <NavLink className={classes.link} activeClassName={classes.active} to={"/contacts"}>
+        <NavLink
+          className={(navData) =>
+            navData.isActive
+              ? `${classes.link} ${classes.active}`
+              : classes.link
+          }
+          to={"/contacts"}
+        >
           Contacts
         </NavLink>
       </nav>
